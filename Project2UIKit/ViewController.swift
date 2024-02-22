9cae31c60086f9adaebdf13865a3de0439e914ce
Sandy Gomez <sgomez1029@gmail.com>
2024-02-22 13:21:19 -0800

    Initial Commit

---
+++ Project2UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2UIKit
+//
+//  Created by Sandra Gomez on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
